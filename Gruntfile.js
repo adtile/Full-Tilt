55a3bb567e793689a63558a06ad2e57685f2953f
richtr <rich.tibbett@gmail.com>
2014-10-10 14:28:40 +0200

    Fix indentation formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,59 +1,50 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        separator: '\n'
-      },
-      dist: {
-        src: [
-            'src/intro.js',
-            'src/Quaternion.js',
-            'src/RotationMatrix.js',
-            'src/Euler.js',
-            'src/DeviceOrientation.js',
-            'src/DeviceMotion.js',
-            'src/outro.js'
-        ],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.title %> v<%= pkg.version %> / <%= pkg.homepage %> */\n'
-      },
-      dist: {
-        files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-        }
-      }
-    },
-    jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-      options: {
-        // options here to override JSHint defaults
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true,
-          document: true
-        }
-      }
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
-    }
-  });
+	grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+	concat: {
+		options: {
+		separator: '\n'
+		},
+		dist: {
+			src: [
+				'src/intro.js',
+				'src/Quaternion.js',
+				'src/RotationMatrix.js',
+				'src/Euler.js',
+				'src/DeviceOrientation.js',
+				'src/DeviceMotion.js',
+				'src/outro.js'
+			],
+			dest: 'dist/<%= pkg.name %>.js'
+		}
+	},
+	uglify: {
+		options: {
+			banner: '/*! <%= pkg.title %> v<%= pkg.version %> / <%= pkg.homepage %> */\n'
+		},
+		dist: {
+			files: {
+				'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+			}
+		}
+	},
+	jshint: {
+		files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+	},
+	watch: {
+		files: ['<%= jshint.files %>'],
+		tasks: ['jshint']
+	}
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('test', ['jshint']);
+	grunt.registerTask('test', ['jshint']);
 
-  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+	grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 
 };
